abaf7277d2661e6f1924e224d30137b3d6b15580
cheonjiwan <giyan1000@naver.com>
2020-08-27 01:05:17 +0900

    Updates

---
+++ precache-manifest.aaaa085659eb36160b345735045cd1ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a99cddd0d0e75f0341de03d4b84a9b",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "0a6a98505a7f4ed8ab07",
+    "url": "/movie_app_2020/static/css/main.b6beb703.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a6a98505a7f4ed8ab07",
+    "url": "/movie_app_2020/static/js/main.0c4a74d4.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
